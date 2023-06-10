453abadfb87963407de3ebff68b1df6bedcf96ef
AdamMahdad <136065266+AdamMahdad@users.noreply.github.com>
2023-06-10 17:49:04 +0200

    Delete javascript directory

--- javascript/I-Love-Pi-3fac6352-e8cc90a2/script.js
+++
@@ -1,8 +0,0 @@
-const diameter = document.getElementById("number");
-const button = document.getElementById("button");
-const haaf = document.getElementById("so");
-
-button.addEventListener("click", (e) => {
-    const waarde = diameter.value * 3.14;
-    haaf.innerText = waarde;
-});
